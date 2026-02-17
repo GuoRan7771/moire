8ea0a628ab338be088bffa2b08723adadb502c02
GuoRan7771 <142179307+GuoRan7771@users.noreply.github.com>
2026-02-17 13:12:48 +0800

    Update moire.config.ts

--- moire.config.ts
+++ moire.config.ts
@@ -1,10 +1,10 @@
 export const config = {
-  title: "Moire",
-  author: "Aeris",
-  theme: "classic",
-  pageSize: 20,
+  title: "说点想说的",
+  author: "Guo",
+  theme: "receipt",
+  pageSize: 10,
   order_by: "created",
-  description: "Sync your thoughts from Apple Notes by Shortcuts.",
+  description: "通过Apple notes同步写点东西，说点想说的，未经审查（包括自我审查）！",
   keywords: "memo, thought, sync, apple notes, shortcuts, ios",
   url: "https://moire.blog"
 };
